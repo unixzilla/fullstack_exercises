7f004e8b5998ae53f6cccd0156270959f6761b89
unix chan <unix@id55.com>
2021-08-01 14:21:18 +0800

    finished 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -20,28 +20,36 @@ const Total = props => {
     props.parts.forEach(part => {total += part.exercises})
     return <p>Number of exercises {total}</p>
 }
-
+const Course = props => {
+    return (
+        <>
+        <Header course={props.course.name} />
+        <Content parts={props.course.parts} />
+        <Total parts={props.course.parts}/>
+        </>
+    )
+}
 const App = () => {
-    const course = 'Half Stact application development'
-    const parts = [
-    {
-        name:'Fundamentals of React',
-        exercises:10
-    },
-    {
-        name :'Using props to pass data',
-        exercises:7
-    },
-    {
-        name : 'State of a component',
-        exercises :14
+    const course = {
+        name:'Half Stact application development',
+        parts:[
+            {
+                name:'Fundamentals of React',
+                exercises:10
+            },
+            {
+                name :'Using props to pass data',
+                exercises:7
+            },
+            {
+                name : 'State of a component',
+                exercises :14
+            }
+        ]
     }
-    ]
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts}/>
+            <Course course={course} />
         </div>
     )
 }
