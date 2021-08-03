915166f855d86d90bff4e610f0a2802366668f1f
unix chan <unix@id55.com>
2021-08-03 17:35:38 +0800

    finished 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,42 @@
 import React,{ useState } from 'react'
+const StatisticLine = ({text,value}) => {
+  return (
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+  )
+}
+const Statistics = ({good,neutral,bad}) => {
+  const all = good+neutral+bad 
+  const average = ((good-bad)>0)?(good-bad)/all :0
+  const positive = (all>0)?(good/all)*100:0
+  
+  if(all > 0){
 
+    return (
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={good + neutral + bad} />
+            <StatisticLine text="average" value={average.toFixed(1) + "%"} />
+            <StatisticLine text="positive" value={positive.toFixed(1) + "%"} />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+    </div>
+  )
+}
 const Button = ({onClick,name}) => <button onClick={onClick} >{name}</button>
 const App = () => {
   const [good,setGood] = useState(0)
@@ -15,6 +52,7 @@ const App = () => {
      <Button name='good' onClick={clickGood} />
      <Button name='neutral' onClick={clickNeutral} />
      <Button name='bad' onClick={clickBad} />
+     <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
