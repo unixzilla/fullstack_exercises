fa92b1193edd8d16b0dc2b39f9619da689640824
unix chan <unix@id55.com>
2021-08-03 16:07:24 +0800

    fisished 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,20 @@
 import React,{ useState } from 'react'
 
+const Button = ({onClick,name}) => <button onClick={onClick} >{name}</button>
 const App = () => {
-  const {good,setGood} = useState(0)
-  const {neutral,setNeutral} = useState(0)
-  const {bad,setBad} = useState(0)
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad,setBad] = useState(0)
+
+  const clickGood = () => setGood(good + 1)
+  const clickNeutral = () => setNeutral(neutral + 1)
+  const clickBad = () => setBad(bad + 1)
   return (
     <div>
-      code here
+     <h1>give feedback</h1> 
+     <Button name='good' onClick={clickGood} />
+     <Button name='neutral' onClick={clickNeutral} />
+     <Button name='bad' onClick={clickBad} />
     </div>
   )
 }
