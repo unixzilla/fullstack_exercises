3fb313395f488683dbb2b3c6229eabbe53eab604
unix chan <unix@id55.com>
2021-08-03 17:56:37 +0800

    finished 1.12

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,31 @@
+import React,{ useState } from "react";
+
+const Button = ({text,onClick}) => {
+  return (
+     <div><button onClick={onClick} >{text}</button></div>
+  )
+}
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const nextAnecdote = () => {
+    const index = Math.floor(Math.random() * anecdotes.length)
+    setSelected(index)
+  }
+  return (
+    <div>
+     {anecdotes[selected]}
+     <Button onClick={nextAnecdote} text="next anecdote" />
+    </div>
+  )
+}
+export default App;
