89d33808b950347ce0e6d81178b68832960a6b44
unix chan <unix@id55.com>
2021-08-06 10:49:23 +0800

    finisih 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,6 +5,39 @@ const Person = ({person}) => {
     <div>{person.name} {person.number}</div>
   )
 }
+const Persons = ({personToShow})=>{
+  return (
+    <div>
+      {personToShow.map((person,i)=><Person key={i} person={person}/>)}
+    </div>
+  )
+}
+
+const Filter = ({newSearch,setSearch}) => {
+  return (
+    <div>
+        filter shown with<input value={newSearch} onChange={(event) => setSearch(event.target.value)} />
+    </div>
+  )
+}
+
+const PersonForm = ({addPerson,newName,newNumber,setNewName,setNewNumber}) => {
+
+  return (
+    <div>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={(event)=>setNewName(event.target.value)}/>
+        </div>
+        <div>number: <input value={newNumber} onChange={(event)=>setNewNumber(event.target.value)}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+    </div>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -33,23 +66,18 @@ const App = () => {
   }) : persons
   return (
     <div>
-      <div>debug: {newName} {newNumber}</div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with<input value={newSearch} onChange={(event) => setSearch(event.target.value)} />
-      </div>
-      <form onSubmit={addPerson}>
-        <h2>Add a new</h2>
-        <div>
-          name: <input value={newName} onChange={(event)=>setNewName(event.target.value)}/>
-        </div>
-        <div>number: <input value={newNumber} onChange={(event)=>setNewNumber(event.target.value)}/></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {personToShow.map((person,i)=><Person key={i} person={person}/>)}
+      <Filter search={newSearch} setSearch={setSearch} />
+      <h3>Add a new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        setNewName={setNewName}
+        setNewNumber={setNewNumber}
+      />
+      <h3>Numbers</h3>
+      <Persons personToShow={personToShow} />
     </div>
   )
 }
