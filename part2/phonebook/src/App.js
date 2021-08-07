6b77e05098641875f353cb0e270095f8460e07e9
unix chan <unix@id55.com>
2021-08-07 10:06:34 +0800

    finished 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,7 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+
+
 
 const Person = ({person}) => {
   return(
@@ -40,12 +43,25 @@ const PersonForm = ({addPerson,newName,newNumber,setNewName,setNewNumber}) => {
 }
 
 const App = () => {
+  /*
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+*/
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        if (response.status === 200) {
+          setPersons(response.data)
+        }
+      })
+  }
+    , [])
   const [ newNumber, setNewNumber ] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newSearch, setSearch ] = useState('')
