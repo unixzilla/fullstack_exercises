863ef6044465f8afd7ce23462d0b221955a884f7
Unix Chan <unix@id55.com>
2021-11-21 02:29:34 +0000

    finished 7.7

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+  useEffect(() => {
+    axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=ture`)
+      .then(response => {
+        setCountry({
+          found:true,
+          data:{
+            name:response.data[0].name.common,
+            capital:response.data[0].capital[0],
+            population:response.data[0].population,
+            flag:response.data[0].flags.png,
+          }
+        }) 
+      })
+      .catch(error => {
+        console.log(error)
+        setCountry({
+          found:false
+        })
+      })
+    },[name])
+    //useEffect hook second parameter array is control the effect function is executed.
+    //So that when the ${name} variable is updated the effect function will execute.
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
